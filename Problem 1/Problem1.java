f07a88b0b1c07b44ce455c5ab92035599179c04b
Himanshu Gupta <himanshu@coseer.com>
2019-09-13 14:02:00 +0530

    added solution for problem 1

--- Problem 1/Problem1.java
+++ Problem 1/Problem1.java
@@ -2,37 +2,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Problem statement:
- * ------------------
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- * <p>
+ *
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- * <p>
+ *
  * Example:
- * <p>
+ *
  * Given nums = [2, 7, 11, 15], target = 9,
- * <p>
+ *
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
  */
-public class TwoSum {
+public class Problem1 {
 
-    /**
-     * Method to return indexes of the two integers whose sum is equal to target
-     *
-     * @param nums
-     * @param target
-     */
-    public int[] twoSum(int[] nums, int target) {
+    private static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> indexMap = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (indexMap.containsKey(complement)) {
-                return new int[]{indexMap.get(complement), i};
+            int j = target - nums[i];
+            if (indexMap.containsKey(j)) {
+                return new int[]{indexMap.get(j), i};
             }
             indexMap.put(nums[i], i);
         }
         return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] sol = twoSum(nums, target);
+        for (int n : sol) {
+            System.out.print(n + " ");
+        }
     }
 }
