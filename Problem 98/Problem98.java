cfc1519f1156ae787da55229609f0c06cc7f0a44
Himanshu Gupta <himanshu@coseer.com>
2021-01-06 08:53:35 +0530

    added solution for problem 98

---
+++ Problem 98/Problem98.java
@@ -0,0 +1,80 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+ *
+ * A valid BST is defined as follows:
+ *
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ *
+ *
+ * Example 1:
+ * Input: root = [2,1,3]
+ * Output: true
+ *
+ * Example 2:
+ * Input: root = [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is 4.
+ *
+ * Constraints:
+ * The number of nodes in the tree is in the range [1, 10^4].
+ * -2^31 <= Node.val <= 2^31 - 1
+ */
+public class Problem98 {
+
+    /**
+     * Inner Node data structure for Trees
+     */
+    private static class Node {
+        Node left, right;
+        int val;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * Iterative approach to traverse tree.
+     * Trees are nothing but beautiful graphs. Hence, dfs approach to traverse is much better. Recursive looks
+     * elegant, but is limited in number of problems it can help solve
+     *
+     * @param root
+     * @return
+     */
+    private static boolean isValidBST(Node root) {
+        if (root == null) { return true; }
+        Deque<Node> stack = new ArrayDeque<>();
+        Node prev = null;
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (prev != null && root.val <= prev.val) {
+                return false;
+            }
+            prev = root;
+            root = root.right;
+        }
+        return true;
+    }
+
+    /**
+     * Main method for test cases
+     * @param args
+     */
+    public static void main(String[] args) {
+        Node root = new Node(2);
+        root.right = new Node(3);
+        root.right.left = new Node(1);
+
+        System.out.println(isValidBST(root));
+    }
+}
