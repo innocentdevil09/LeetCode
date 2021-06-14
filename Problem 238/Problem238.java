24f2d05d1a0521bb20b7817c7e12516585e1a07d
Himanshu Gupta <himanshu@coseer.com>
2021-06-14 14:58:02 +0530

    added solution for problem 238

---
+++ Problem 238/Problem238.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+
+/**
+ * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the
+ * elements of nums except nums[i].
+ *
+ * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ *
+ * You must write an algorithm that runs in O(n) time and without using the division operation.
+ *
+ * Example 1:
+ * Input: nums = [1,2,3,4]
+ * Output: [24,12,8,6]
+ *
+ * Example 2:
+ * Input: nums = [-1,1,0,-3,3]
+ * Output: [0,0,9,0,0]
+ *
+ * Constraints:
+ * 2 <= nums.length <= 10^5
+ * -30 <= nums[i] <= 30
+ * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ *
+ * Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra
+ * space for space complexity analysis.)
+ */
+public class Problem238 {
+
+    /**
+     * Two for loops to get product of all nums at index i except self
+     *
+     * Time Complexity: O(N)
+     * Space Complexity: O(1)
+     *
+     * @param nums
+     * @return
+     */
+    private static int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        output[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            output[i] = output[i - 1] * nums[i - 1];
+        }
+
+        int R = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            output[i] *= R;
+            R *= nums[i];
+        }
+        return output;
+    }
+
+    /**
+     * Main method for test cases
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] nums = {-1,1,0,-3,3};
+        int[] output = productExceptSelf(nums);
+        Arrays.stream(output).forEach(n -> System.out.print(n + " "));
+    }
+}
