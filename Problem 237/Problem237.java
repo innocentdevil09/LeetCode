ca6b2d94b028d940626c71495aa8a1d8451b0d8f
Himanshu Gupta <himanshu@coseer.com>
2021-06-14 11:10:21 +0530

    added solution for problem 237

---
+++ Problem 237/Problem237.java
@@ -0,0 +1,84 @@
+/**
+ * Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list,
+ * instead you will be given access to the node to be deleted directly.
+ *
+ * It is guaranteed that the node to be deleted is not a tail node in the list.
+ *
+ * Example 1:
+ * Input: head = [4,5,1,9], node = 5
+ * Output: [4,1,9]
+ * Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling
+ * your function.
+ *
+ * Example 2:
+ * Input: head = [4,5,1,9], node = 1
+ * Output: [4,5,9]
+ * Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling
+ * your function.
+ *
+ * Example 3:
+ * Input: head = [1,2,3,4], node = 3
+ * Output: [1,2,4]
+ *
+ * Example 4:
+ * Input: head = [0,1], node = 0
+ * Output: [1]
+ *
+ * Example 5:
+ * Input: head = [-3,5,-99], node = -3
+ * Output: [5,-99]
+ *
+ * Constraints:
+ * The number of the nodes in the given list is in the range [2, 1000].
+ * -1000 <= Node.val <= 1000
+ * The value of each node in the list is unique.
+ * The node to be deleted is in the list and is not a tail node
+ */
+public class Problem237 {
+
+    /**
+     * Inner Node class to represent Linked List data structure
+     */
+    private static class Node {
+        int val;
+        Node next;
+
+        Node(int val) { this.val = val; }
+    }
+
+    /**
+     * Method to delete given node from the linked list
+     *
+     * Time Complexity: O(1)
+     * Space Complexity: O(1)
+     *
+     * @param node
+     */
+    private static void deleteNode(Node node) {
+        if (node == null) { return; }
+        if (node.next == null) {
+            node = null;
+            return;
+        }
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    /**
+     * Main method for test cases
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Node head = new Node(4);
+        head.next = new Node(5);
+        head.next.next = new Node(1);
+        Node toDelete = new Node(9);
+        head.next.next.next = toDelete;
+
+        deleteNode(toDelete);
+        for (Node x = head; x != null; x = x.next) {
+            System.out.print(x.val + " ");
+        }
+    }
+}
