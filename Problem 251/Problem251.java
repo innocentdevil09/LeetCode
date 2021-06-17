094c27812fd20afe54ae577326727f99ec58c64c
Himanshu Gupta <himanshu@coseer.com>
2021-06-17 16:07:51 +0530

    added solution for problem 251

---
+++ Problem 251/Problem251.java
@@ -0,0 +1,88 @@
+/**
+ * Design an iterator to flatten a 2D vector. It should support the next and hasNext operations.
+ *
+ * Implement the Vector2D class:
+ *
+ * Vector2D(int[][] vec) initializes the object with the 2D vector vec.
+ * next() returns the next element from the 2D vector and moves the pointer one step forward. You may assume that all
+ * the calls to next are valid.
+ * hasNext() returns true if there are still some elements in the vector, and false otherwise.
+ *
+ * Example 1:
+ * Input
+ * ["Vector2D", "next", "next", "next", "hasNext", "hasNext", "next", "hasNext"]
+ * [[[[1, 2], [3], [4]]], [], [], [], [], [], [], []]
+ * Output
+ * [null, 1, 2, 3, true, true, 4, false]
+ *
+ * Explanation
+ * Vector2D vector2D = new Vector2D([[1, 2], [3], [4]]);
+ * vector2D.next();    // return 1
+ * vector2D.next();    // return 2
+ * vector2D.next();    // return 3
+ * vector2D.hasNext(); // return True
+ * vector2D.hasNext(); // return True
+ * vector2D.next();    // return 4
+ * vector2D.hasNext(); // return False
+ *
+ * Constraints:
+ * 0 <= vec.length <= 200
+ * 0 <= vec[i].length <= 500
+ * -500 <= vec[i][j] <= 500
+ * At most 10^5 calls will be made to next and hasNext.
+ *
+ * Follow up: As an added challenge, try to code it using only iterators in C++ or iterators in Java.
+ */
+public class Problem251 {
+
+    /**
+     * Vector2D class to iterate over elements in the given matrix
+     */
+    private static class Vector2D {
+
+        private int[][] inputData;
+        private int row = 0;
+        private int col = 0;
+
+        public Vector2D(int[][] v) {
+            this.inputData = v;
+        }
+
+        public int next() {
+            if (hasNext()) {
+                int val = inputData[row][col];
+                col++;
+                return val;
+            }
+            return -1;
+        }
+
+        public boolean hasNext() {
+            while (row < inputData.length) {
+                if (col >= inputData[row].length) {
+                    row++;
+                    col = 0;
+                    continue;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Main method for test cases
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Vector2D vector2D = new Vector2D(new int[][]{{1, 2}, {3}, {4}});
+        System.out.println(vector2D.next());    // return 1
+        System.out.println(vector2D.next());    // return 2
+        System.out.println(vector2D.next());    // return 3
+        System.out.println(vector2D.hasNext()); // return True
+        System.out.println(vector2D.hasNext()); // return True
+        System.out.println(vector2D.next());    // return 4
+        System.out.println(vector2D.hasNext()); // return False
+    }
+}
